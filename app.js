3a669ee2be36156c0920e9489f518efda2b3f86a
Mkh <example@gmail.com>
2025-08-27 07:34:03 +0200

    creating-&-storing-user

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
 
+const User = require("./models/user");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -18,13 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  /*   User.findByPk(1)
+  User.findById("0000000")
     .then((user) => {
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err)); */
-  next();
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
--- models/user.js
+++ models/user.js
@@ -1,16 +1,22 @@
-const Sequelize = require('sequelize');
+const getDb = require("../util/database").getDb;
+const { ObjectId } = require("mongodb");
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+  save() {
+    const db = getDb();
+    return db.collection("users").insertOne(this);
+  }
 
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  static findById(id) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: ObjectId.createFromHexString(id) })
+      .catch((err) => console.log(err));
+  }
+}
 
 module.exports = User;
